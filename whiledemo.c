0574ee2e23102fa6e7dbf6cd73b81838145d47bf
Deep21Patel <111433307+DharmeshPatel007@users.noreply.github.com>
2023-03-02 16:10:08 +0530

    2-3-23
    sdsdfsd

---
+++ whiledemo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+    while :
+
+    syntax:
+
+            intialization
+
+            while(condition)
+            {
+                statement
+                updation
+            }
+
+    - while loop is called as Entry Controlled Loop.
+    - Because the condition is checked first and then the body is
+      executed.
+    - No semicolon after while condition.
+
+*/
+
+void main()
+{
+    int i = 0; // initialization state.
+
+    while (345)
+    {
+        printf("Hello");
+    }
+}
